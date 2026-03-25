7ce8600909ceec93da63cd9d9e26e6cac0089ac4
Guilherme de Oliveira Nascimento <g.oliveiranascimento096@gmail.com>
2026-03-25 00:33:25 -0300

    Added facades/url to ensure https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -14,6 +14,7 @@ use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Validation\Rules\Password;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -30,6 +31,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        if (app()->isProduction()) {
+            URL::forceScheme('https');
+        }
+
         Paginator::useTailwind();
 
         $this->configureDefaults();
